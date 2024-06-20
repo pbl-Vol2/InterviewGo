acd7b23fdfa3526e079ffb562d8364ab90dddd94
kaniraerlna <kanira.zalea@gmail.com>
2024-06-20 14:49:37 +0700

    update regist and landing

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -9,13 +9,19 @@ const Landing = () => {
           <img src={logo} alt="Logo" className="h-48 w-auto" />
         </div>
         <h1 className="text-5xl font-extrabold text-gray-900 mb-4 leading-tight">
-          Helps you prepare for <span className="text-customBiru3">job interviews</span> with confidence and top-notch skills.
+          Helps you prepare for{" "}
+          <span className="text-customBiru3">job interviews</span> with
+          confidence and top-notch skills.
         </h1>
         <p className="text-2xl text-gray-600 mb-6 max-w-3xl mx-auto">
-          With our advanced features, you will be prepared for every question and challenge in the job selection process.
+          With our advanced features, you will be prepared for every question
+          and challenge in the job selection process.
         </p>
         <div className="mt-8 flex justify-center">
-          <button className="mt-6 bg-gradient-to-r from-customBiru3 to-customBiru6 text-white py-3 px-8 rounded-full shadow-lg transform transition-transform hover:scale-105 hover:from-customBiru4 hover:to-customBiru3">
+          <button
+            // onClick={handleSignupButton}
+            className="mt-6 outline outline-offset-2 outline-4 outline-customBiru3 bg-gradient-to-r from-customBiru3 to-customBiru6 text-white py-3 px-8 rounded-full shadow-lg transform transition-transform hover:scale-105 hover:from-customBiru4 hover:to-customBiru3"
+          >
             Get Started
           </button>
         </div>
